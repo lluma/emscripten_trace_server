8d42861262e3ff800375eee5d1764fed6ba505ac
rex_wang <rex_wang@perfectcorp.com>
2023-02-07 16:08:25 +0800

    fix some error occurred by old syntax from python2

--- trace_collector/__init__.py
+++ trace_collector/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from trace_collector import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.json_encoder = json.TraceCollectorJSONEncoder
 
 import trace_collector.api
